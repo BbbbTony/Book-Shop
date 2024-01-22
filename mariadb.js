4a3f391c5fcedf28e379e1f661cd9c9df68dfe05
Tony <gogo6570@gmail.com>
2024-01-22 22:00:53 +0900

    feat : routes 생성

---
+++ mariadb.js
@@ -0,0 +1,12 @@
+const mariadb = require('mysql2');
+
+const connection = mariadb.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: 'root',
+    database: 'Bookshop',
+    dateStrings: true,
+});
+
+module.exports = connection;
