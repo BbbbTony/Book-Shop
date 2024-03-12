2e66c4ed6170e4c98cb413b103715d097175a762
Tony <gogo6570@gmail.com>
2024-03-12 21:47:36 +0900

    feat: corsOption 구현

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.listen(process.env.PORT);
-
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+};
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
 const categoryRouter = require('./routes/category');
@@ -13,9 +15,12 @@ const likeRouter = require('./routes/likes');
 const cartRouter = require('./routes/carts');
 const orderRouter = require('./routes/orders');
 
+app.use(cors(corsOptions));
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
 app.use('/category', categoryRouter);
 app.use('/likes', likeRouter);
 app.use('/carts', cartRouter);
 app.use('/orders', orderRouter);
+
+app.listen(process.env.PORT);
