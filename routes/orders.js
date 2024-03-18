f3a4b8a0cfd1027d7640a766f127ebc81fcad463
Tony <gogo6570@gmail.com>
2024-03-19 04:15:07 +0900

    fix: 프리티어 수정

--- routes/orders.js
+++ routes/orders.js
@@ -1,22 +1,21 @@
-const express =require('express');
+const express = require('express');
 const router = express.Router();
 
 router.use(express.json());
 
 //주문하기
-router.post('/:id', (req,res) => {
+router.post('/:id', (req, res) => {
     res.json('주문하기');
 });
 
 //주문 목록 조회
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.json('주문 목록 조회');
 });
 
 //주문 상세 상품 조회
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res) => {
     res.json('주문 상세 상품 조회');
 });
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../mariadb');
-const {StatusCodes} = require('http-status-codes');
-const {
-    join, 
-    login, 
-    passwordReset, 
-    passwordResetRequest
-} = require('../controller/UserController');
+const { StatusCodes } = require('http-status-codes');
+const { join, login, passwordReset, passwordResetRequest } = require('../controller/UserController');
 
-
-// 회원가입 
+// 회원가입
 router.use(express.json());
 router.post('/join', join);
 
-//로그인 
-router.post('/login',login);
+//로그인
+router.post('/login', login);
 
 //비밀번호 초기화 요청
 router.post('/reset', passwordResetRequest);
 
 // 비밀번호 초기화
 router.put('/reset', passwordReset);
-
 
 module.exports = router;
